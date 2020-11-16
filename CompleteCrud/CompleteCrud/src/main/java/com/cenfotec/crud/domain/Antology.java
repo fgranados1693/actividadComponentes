40b3b9ace2bc3bd6e38221910b7cdd9fbeae485f
fgranados1693 <52337176+fgranados1693@users.noreply.github.com>
2020-11-15 19:19:34 -0600

    todo la practica de examen
    Práctica de examen completa

---
+++ CompleteCrud/CompleteCrud/src/main/java/com/cenfotec/crud/domain/Antology.java
@@ -0,0 +1,71 @@
+package com.cenfotec.crud.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Antology {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+	public long id;	
+	private String name;
+	private String subtitulo;
+	private String fechaPublicacion;
+	
+	public String getSubtitulo() {
+		return subtitulo;
+	}
+
+	public void setSubtitulo(String subtitulo) {
+		this.subtitulo = subtitulo;
+	}
+
+	public String getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+
+	public void setFechaPublicacion(String fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="anthology")
+	
+	private Set<Article> articles;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(Set<Article> articles) {
+		this.articles = articles;
+	}
+	
+	
+}
